2a73c08847c583d826d7d082cbedb1e8e50bf4d3
Gaspard Hoyaux <ghoyaux@c1r6p3.42lehavre.fr>
2024-08-08 02:08:44 +0200

    C00 --> ex00 to ex04 added succesfuly

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghoyaux <ghoyaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 17:55:56 by ghoyaux           #+#    #+#             */
+/*   Updated: 2024/08/07 18:04:47 by ghoyaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
