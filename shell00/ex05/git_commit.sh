6e2bdf25ca41fce2488a75ec3734c15cf2812852
Gaspard Hoyaux <ghoyaux@c1r3p4.42lehavre.fr>
2024-08-08 19:02:37 +0200

    Project C01 (ex00 to ex07) added succesfully

--- shell00/ex05/git_commit.sh
+++ shell00/ex05/git_commit.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-# Change le répertoire de travail pour être un niveau au-dessus du répertoire actuel
-cd "$(dirname "$PWD")" || exit
-
-# Vérifie si le répertoire actuel est un répertoire Git
-if [ ! -d ".git" ]; then
-  echo "Le répertoire actuel n'est pas un dépôt Git."
-  exit 1
-fi
-
-# Affiche les 5 derniers identifiants de commit depuis le répertoire actuel
-echo "Les 5 derniers commits dans le répertoire $(pwd) sont :"
 git log -n 5 --pretty=format:"%H"
