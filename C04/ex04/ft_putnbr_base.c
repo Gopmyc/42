1f84901b4d8465857a06d3272c170a4dad4dc0cd
Gaspard Hoyaux <ghoyaux@c1r6p3.42lehavre.fr>
2024-08-11 05:42:33 +0200

    Added C04 (ex00 to ex04)

---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghoyaux <ghoyaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 05:39:26 by ghoyaux           #+#    #+#             */
+/*   Updated: 2024/08/11 05:41:11 by ghoyaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>  // Pour la fonction write
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	ft_check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) <= 1)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	nb;
+	int		base_len;
+
+	if (!ft_check_base(base))
+		return ;
+	nb = nbr;
+	base_len = ft_strlen(base);
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= base_len)
+		ft_putnbr_base(nb / base_len, base);
+	ft_putchar(base[nb % base_len]);
+}
